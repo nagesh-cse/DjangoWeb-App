5263bbc7b28224b6af0d79343443328c82fab28d
nagesh-cse <88593561+nagesh-cse@users.noreply.github.com>
2022-07-23 06:53:38 +0530

    Update views.py

--- ShopVibes/realstore/views.py
+++ ShopVibes/realstore/views.py
@@ -1,14 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
-from .models import product
-from .forms import CustomerRegistrationForm, LoginForm
+from .models import cart, customer, product
+from .forms import CustomerProfileForm, CustomerRegistrationForm, LoginForm
 from django.contrib import messages
 # Create your views here.
 # def home(request):
 #     return render(request,'realstore/home.html')
-
-# hello
 
 class ProductView(View):
     
@@ -51,7 +49,34 @@ class productdetailview(View):
 
 
 def addtocart(request):
-    return render(request,'realstore/addtocart.html')
+    user=request.user
+    product_id=request.GET.get('prod_id')
+    Product = product.objects.get(id=product_id)
+    cart(user=user, product=Product).save()
+    return redirect('/cart')
+    
+
+def showcart(request):
+    if request.user.is_authenticated:
+        user=request.user
+        Cart=cart.objects.filter(user=user)
+        total_amount=0.0
+        amount=0.0
+        shipping_amount=50.0
+        if Cart:
+            for p in Cart:
+                if p.user==user:
+                    amount=p.product.discounted_price+amount
+                    total_amount=amount+shipping_amount
+            return render(request, 'realstore/addtocart.html', {'carts': Cart, 'amount': amount, 'total_amount': total_amount, 'shipping_amount': shipping_amount})
+        else:
+            return render(request, 'realstore/emptycart.html')
+        
+        
+        
+        
+        
+        
 
 # def customerregistration(request):
 #     return render(request,'realstore/customerregistration.html')
@@ -74,13 +99,33 @@ class CustomerRegistrationView(View):
 #         return render(request,'realstore/login.html',{'form':form})
 
 def address(request):
-    return render(request,'realstore/address.html')
+    address = customer.objects.get(user = request.user)
+    return render(request,'realstore/address.html',{'address':address,'active': 'btn-primary'})
 
 def checkout(request):
     return render(request,'realstore/checkout.html')
 
 def buynow(request):
     return render(request,'realstore/buynow.html')
+
+class ProfileView(View):
+    def get(self, request):
+        form = CustomerProfileForm()    
+        return render(request, 'realstore/profile.html', {'form': form, 'active': 'btn-primary'})
+
+    def post(self, request):
+        form=CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user=request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+            data = customer(user=user, name=name, locality=locality, city=city, state=state, zipcode=zipcode)
+            data.save()
+            messages.success(request, 'Profile Updated Succesfully!')
+        return render(request, 'realstore/profile.html', {'form': form, 'active': 'btn-primary'})
 
 
 
